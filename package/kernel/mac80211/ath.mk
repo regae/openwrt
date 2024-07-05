feeddbb546589b4836aafbf6f4d6ec409cd12376
regae <37059068+regae@users.noreply.github.com>
2024-07-05 21:09:50 +0700

    mac80211: ath10k: enabling smart antenna as default
    for testing
    echo 0x800000 > /sys/module/ath10k_core/parameters/debug_mask
    
    ath10k_pci 0001:01:00.0: Training completed:antenna selected 4 PER 54 RSSI 37 34 31 rate e8e5e5, max peers 116, max sta 100
    ath10k_pci 0001:01:00.0: Configure tx antenna for XX:XX:XX:XX:XX:XX to 4
    ath10k_pci 0001:01:00.0: Configure Rx antenna to 5
    ath10k_pci 0001:01:00.0: start Periodic train

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -268,7 +268,7 @@ define KernelPackage/ath10k
 	$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath10k/ath10k_core.ko \
 	$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath10k/ath10k_pci.ko
   AUTOLOAD:=$(call AutoProbe,ath10k_core ath10k_pci)
-  MODPARAMS.ath10k_core:=frame_mode=2
+  MODPARAMS.ath10k_core:=frame_mode=2 enable_smart_antenna=1
   VARIANT:=regular
 endef
 
